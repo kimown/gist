e5028ef47eecc1b76f49e9813f7bfbb326bdd431
kimown <kimown@163.com>
2016-09-22 22:40:35 +0800

    node

---
+++ docker/node/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send(`${process.version}`);
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
