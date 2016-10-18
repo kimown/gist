93bdf6f866d37d8a5514ddf38bd68dd2896fd617
kimown <kimown@163.com>
2016-10-18 12:05:21 +0800

    init

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+var a=1;
