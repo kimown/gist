c974014f4a1e144a87df8044d0783c5348af09e9
kimown <kimown@163.com>
2016-10-29 14:13:36 +0800

    Add: webpack , webpack-dev-server

---
+++ webpack-dev-server-demo/webpck.config.js
@@ -0,0 +1,13 @@
+var path = require("path");
+
+console.log(path.resolve(__dirname, "build"));
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  }
+};
