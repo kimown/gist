06f81f2494f29de04906e494172c7cdccccab112
kimown <kimown@163.com>
2016-11-09 15:05:52 +0800

    clear console info

--- react-swipeable/src/autoplay.js
+++ react-swipeable/src/autoplay.js
@@ -11,11 +11,18 @@ import mod from "react-swipeable-views/lib/utils/mod";
 const EnhancedSwipeableViews = virtualize(SwipeableViews);
 
 
-class Autoplay extends Component {
+class App extends Component {
+
     constructor(props) {
         super(props);
-        this.state = {index:0};
+        this.state = {index: 0};
     }
+
+    static defaultProps = {
+        autoplay: true,
+        index: 0,
+        interval: 3000,
+    };
 
     componentWillUnmount() {
         clearInterval(this.timer);
@@ -23,8 +30,85 @@ class Autoplay extends Component {
 
     timer = null;
 
+    slideRenderer = (params, children)=> {
+        let childrenLength = children.length;
+        const {index, key} = params;
+        if (mod(index, childrenLength) < childrenLength) {
+            return this.dealChild(children[mod(index, childrenLength)], key)
+        }
+        return null
+    }
+
+    middle = (params)=> {
+        let {children}=this.props;
+        return this.slideRenderer(params, children);
+    }
 
 
+    dealChild = (div, key)=> {
+        return <div key={key}>
+            {div}
+        </div>
+    }
+
+    componentDidMount() {
+        this.startInterval();
+    }
+
+
+    startInterval() {
+        let {autoplay, interval}=this.props;
+        if (!autoplay) {
+            return
+        }
+        clearInterval(this.timer);
+        this.timer = setInterval(this.handleInterval, interval);
+    }
+
+    handleInterval = () => {
+        let indexLatest = this.state.index;
+        let {children}=this.props;
+        let indexNew = mod((indexLatest + 1), children.length);
+        this.setState({
+            index: indexNew,
+        });
+    };
+
+
+    onChangeIndex = (index, indexLatest)=> {
+        let stateIndex = this.state.index;
+        if ((index - indexLatest) > 0) {
+            stateIndex = (stateIndex + 1);
+        } else if ((index - indexLatest) < 0) {
+            stateIndex = (stateIndex - 1);
+        }
+        this.setState({
+            index: stateIndex
+        })
+    }
+
+    handleSwitching = (index, type) => {
+        if (this.timer) {
+            clearInterval(this.timer);
+            this.timer = null;
+        } else if (type === 'end') {
+            this.startInterval();
+        }
+
+    };
+
+
+    render() {
+        let {index}=this.state;
+        let {children, autoplay, interval}=this.props;
+        return (
+            <EnhancedSwipeableViews
+                index={index}
+                onSwitching={this.handleSwitching}
+                onChangeIndex={this.onChangeIndex}
+                slideRenderer={this.middle}/>
+        );
+    }
 }
 
 
