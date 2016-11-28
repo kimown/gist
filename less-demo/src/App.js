d517e4847c9a138a81b345e334f5018388e69cb0
kimown <kimown@163.com>
2016-11-28 10:38:02 +0800

    feat(git): 2333333333

--- less-demo/src/App.js
+++ less-demo/src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         el.css({'margin-top':marginTop+paddintTop})
         el.css({'padding-bottom':0})
         el.css({'margin-bottom':marginTop+paddintTop})
-        el.height(233)
+        el.height(233333333333333333333333333333333333)
 
 
 
