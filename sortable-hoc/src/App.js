d1950f856db192d6b2af07f2ed6eac18ad4c64f7
kimown <kimown@163.com>
2016-12-14 23:21:12 +0800

    feat(drag): es6

--- sortable-hoc/src/App.js
+++ sortable-hoc/src/App.js
@@ -2,36 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {render} from 'react-dom';
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 
+import Sortable from 'sortablejs';
 
-const SortableItem = SortableElement(({value}) => <li>{value}</li>);
+class SortableComponent extends React.Component {
 
-const SortableList = SortableContainer(({items}) => {
-    return (
-        <ul>
-            {items.map((value, index) =>
-                <SortableItem key={`item-${index}`} index={index} value={value} />
-            )}
-        </ul>
-    );
-});
-
-class SortableComponent extends Component {
-    state = {
-        items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']
-    }
-    onSortEnd = ({oldIndex, newIndex}) => {
-        console.info(`oldIndex= ${oldIndex}, newIndex = ${newIndex}`)
-        this.setState({
-            items: arrayMove(this.state.items, oldIndex, newIndex)
-        });
+    sortableContainersDecorator = (componentBackingInstance) => {
+        console.info('======sortableContainersDecorator')
+        // check if backing instance not null
+        if (componentBackingInstance) {
+            let options = {
+                handle: ".group-title" // Restricts sort start click/touch to the specified element
+            };
+            Sortable.create(componentBackingInstance, options);
+        }
     };
+
+    sortableGroupDecorator = (componentBackingInstance) => {
+        console.info('-----sortableGroupDecorator')
+        // check if backing instance not null
+        if (componentBackingInstance) {
+            let options = {
+                draggable: "div", // Specifies which items inside the element should be sortable
+                group: "shared"
+            };
+            Sortable.create(componentBackingInstance, options);
+        }
+    };
+    handleSort = ()=>{
+        alert(11)
+    }
+
     render() {
         return (
-            <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />
-        )
+            <div className="container">
+                <div className="group">
+                    <h2 className="group-title">Group 1</h2>
+                    <div className="group-list" ref={this.sortableGroupDecorator}>
+                        <div>Swap them around000000000</div>
+                        <div>Swap us around1111111111</div>
+                        <div>Swap things around22222222222</div>
+                        <div>Swap everything around3333333333</div>
+                    </div>
+                </div>
+                <div className="group">
+                    <h2 className="group-title">Group 2</h2>
+                    <div className="group-list" ref={this.sortableGroupDecorator}>
+                        <div>Swap them around0000000000</div>
+                        <div>Swap us around111111111</div>
+                        <div>Swap things around2222222222</div>
+                        <div>Swap everything around33333333333</div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
