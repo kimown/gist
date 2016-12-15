6aac4435757c8dc5f684357b636494f5c67cf4c9
kimown <kimown@163.com>
2016-12-15 21:00:57 +0800

    feat(drag):  sortablejs

--- sortable-hoc/src/App.js
+++ sortable-hoc/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import {render} from 'react-dom';
 import Sortable from 'sortablejs';
+var _ = require('lodash');
 import SortableMixin from 'react-mixin-sortablejs'
 
 
@@ -16,17 +17,94 @@ class SortableExampleEsnext extends React.Component {
             };
             Sortable.create(componentBackingInstance, options);
         }
-    };
+    }
+
+    _onStart = (evt)=> {
+        console.info(`onStart  evt.oldIndex ${ evt.oldIndex}`)
+    }
+
+    _onEnd = (evt) => {
+        console.info(`onEnd evt.oldIndex ${evt.oldIndex} evt.newIndex  ${evt.newIndex}`)
+    }
 
     sortableGroupDecorator = (componentBackingInstance) => {
         // check if backing instance not null
         if (componentBackingInstance) {
             let options = {
                 draggable: "div", // Specifies which items inside the element should be sortable
-                // group: "shared",
-                // onStart: (evt)=>{
-                //    this.onStart()
-                // }
+                forceFallback:true,
+
+                onStart: (evt)=>{
+                    this._onStart(evt)
+                },
+                onEnd: (evt) => {
+                    this._onEnd(evt)
+                },
+
+                // Element is chosen
+                onChoose: function (/**Event*/evt) {
+                    // evt.oldIndex;  // element index within parent
+                    console.info('onChoose')
+                },
+
+                // Element is dropped into the list from another list
+                onAdd: function (/**Event*/evt) {
+                    var itemEl = evt.item;  // dragged HTMLElement
+                    // evt.from;  // previous list
+                    // + indexes from onEnd
+                    console.info('onAdd')
+                },
+
+                // Changed sorting within list
+                onUpdate: function (/**Event*/evt) {
+                    var itemEl = evt.item;  // dragged HTMLElement
+                    // + indexes from onEnd
+                    console.info('onUpdate')
+
+                },
+
+                // Called by any change to the list (add / update / remove)
+                onSort: function (/**Event*/evt) {
+                    // same properties as onUpdate
+                    console.info('onSort')
+
+                },
+
+                // Element is removed from the list into another list
+                onRemove: function (/**Event*/evt) {
+                    // same properties as onUpdate
+                    console.info('onRemove')
+
+                },
+
+                // Attempt to drag a filtered element
+                onFilter: function (/**Event*/evt) {
+                    var itemEl = evt.item;  // HTMLElement receiving the `mousedown|tapstart` event.
+                    console.info('onFilter')
+
+                },
+
+                // Event when you move an item in the list or between lists
+                onMove: function (/**Event*/evt, /**Event*/originalEvent) {
+                    // Example: http://jsbin.com/tuyafe/1/edit?js,output
+                    // evt.dragged; // dragged HTMLElement
+                    // evt.draggedRect; // TextRectangle {left, top, right и bottom}
+                    // evt.related; // HTMLElement on which have guided
+                    // evt.relatedRect; // TextRectangle
+                    // originalEvent.clientY; // mouse position
+                    // return false; — for cancel
+                    console.info('onMove')
+
+                },
+
+                // Called when creating a clone of element
+                onClone: function (/**Event*/evt) {
+                    var origEl = evt.item;
+                    var cloneEl = evt.clone;
+                    console.info('onClone')
+
+                },
+
                 handle: '.my-handle',
             };
             Sortable.create(componentBackingInstance, options);
@@ -35,8 +113,12 @@ class SortableExampleEsnext extends React.Component {
 
     constructor(props: PropsType) {
         super(props)
+        const initArray = _.range(0, props.children.length)
+        console.info(`initArray ${initArray}`)
         this.state = {
-            data: ['Swap them around00000','Swap us around111111','Swap things around2222222222','Swap everything around3333333333']
+            data: ['Swap them around00000','Swap us around111111','Swap things around2222222222','Swap everything around3333333333'],
+            lastOrderList:initArray,
+            orderList: initArray,
         }
     }
 
@@ -68,33 +150,20 @@ class SortableExampleEsnext extends React.Component {
     }
 
     render() {
-        const data = this.state.data
-        const divS = data.map((v)=>{
-            return <div>
+        const {children} = this.props
+        const divS = children.map((v)=>{
+            return <div className="sortableItem">
                         <span className="my-handle">{'DRAG ME'}</span>
                         {v}
                     {',,,,,,'}
                     </div>
         })
 
-        const lis = data.map((v)=>{
-            return <li>
-                <span className="my-handle">{'DRAG ME'}</span>
-                {v}
-                {',,,,,,'}
-            </li>
-        })
-
         return (
-            <div className="container">
-                <div className="group">
-                    <h2 className="group-title">Group 1</h2>
-                        <ul id="listWithHandle" ref={this.sortableGroupDecorator}>
-                            {divS}
-                            {/*<li><span className="my-handle">::</span> list item text one</li>*/}
-                            {/*<li><span className="my-handle">::</span> list item text two</li>*/}
-                        </ul>
-                </div>
+            <div id="listWithHandle" ref={this.sortableGroupDecorator}>
+                {divS}
+
+                <div className="addme">Add Me</div>
             </div>
         );
     }
--- sortable-hoc/src/index.js
+++ sortable-hoc/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+
+const children=['Swap them around00000','Swap us around111111','Swap things around2222222222','Swap everything around3333333333']
 ReactDOM.render(
-  <App />,
+  <App children={children} />,
   document.getElementById('root')
 );
